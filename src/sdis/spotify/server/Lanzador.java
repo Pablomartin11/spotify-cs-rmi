68727bfa08063278ebdc91eb8b74dabf65538eb9
Pablomartin11 <pablo.martin.benito@estudiantes.uva.es>
2024-05-07 20:22:37 +0200

    Adaptar protocolo de comunicación

--- src/sdis/spotify/server/Lanzador.java
+++ src/sdis/spotify/server/Lanzador.java
@@ -4,11 +4,13 @@ public class Lanzador {
     public static void main(String [] args){
         try{
             // Declaracion de objetos remotos
+            Spotify spotify = new SpotifyImpl();
 
             // Accedemos a una referencia al registro (rmiregistry) local
             java.rmi.registry.Registry registro = java.rmi.registry.LocateRegistry.getRegistry("localhost");
 
             // Bindear objetos remotos a registry
+            registro.rebind("spotify", spotify);
 
         } catch (Exception e){
             System.err.println("Excepción del servidor: "+e.toString());
