75f683395e94435f3c59b3f91464136488c35260
pablo79d8 <pablo.arriba@estudiantes.uva.es>
2024-05-08 19:49:04 +0200

    Paquete media creado

---
+++ src/sdis/spotify/media/Globals.java
@@ -0,0 +1,39 @@
+package sdis.spotify.media;
+
+public class Globals {
+    public static final String hello_banner =
+                    "███████╗ ██████╗     ██████╗   ████████╗██╗ ███████╗ ██╗     ██╗\n" +
+                    "██╔════╝ ██╔══██╗  ██╔═══  ██╗ ╚══██╔══╝██║ ██╔════╝ ╚██╗   ██╔╝\n" +
+                    "███████╗ ██████╔╝ ██║       ██║   ██║   ██║ █████╗     ╚████╔╝ \n" +
+                    "╚════██║ ██╔═══╝   ██║     ██║    ██║   ██║ ██╔══╝      ╚██╔╝ \n" +
+                    "███████║ ██║        ╚██████╔╝     ██║   ██║ ██║          ██║ \n" +
+                    "╚══════╝ ╚═╝        ╚═════╝       ╚═╝   ╚═╝ ╚═╝          ╚═╝ \n";
+    // Remote object setup
+    public static String remote_name = "RemoteSpotify";
+    // Log paths & msgs
+    public static String log_path = "./logs/";
+    public static String log_extension = ".txt";
+    public static String log_listdr = " [LISTDR] List directory requested";
+    public static String log_readpl = " [READPL] Pull from playlist Media: ";
+    public static String log_peekpl = " [PEEKPL] Peek from playlist Media: ";
+    public static String log_errorm = " [ERRORM] Error Media not found: ";
+    public static String log_stream = " [STREAM] Streaming to: ";
+    public static String log_stream_end = " [STREAM] Stream finished. Tx Bytes: ";
+    // Streaming paths
+    public static String server_host = "localhost";
+    public static int server_port = 20099;
+    public static String path_origin = "./mp3files/origin/";
+    public static String path_destination = "./mp3files/destination/";
+    // Streaming media player config
+    public static int player_delay_ms = 3000;
+    public static String file_extension = ".mp3";
+    public static String player_command = "firefox";
+    //public static String player_command = "vlc";
+    public static String player_abs_filepath = "./mp3files/destination/";
+    // WIN CONFIGS
+//public static String player_command = "wmplayer";
+//public static String player_abs_filepath = "E:\\spotifyL4\\mp3files\\destination\\";
+// Streaming setup
+    public static int tx_packet_size_bytes = 32752;
+    public static int delivery_delay_ms = 50;
+}
---
+++ src/sdis/spotify/media/MediaPlayer.java
@@ -0,0 +1,29 @@
+package sdis.spotify.media;
+
+import java.io.IOException;
+/**
+ * Runnable class MediaPlayer 2024
+ * @author hector
+ */
+public class MediaPlayer implements Runnable{
+    private String exec_command;
+    private String file_path;
+    private int starting_delay;
+    public MediaPlayer(String exec_command, String file_path, int starting_delay){
+        this.exec_command = exec_command;
+        this.file_path = file_path;
+        this.starting_delay = starting_delay;
+    }
+    public void run(){
+        try {
+            System.err.println("** Launching "+exec_command+" w/ delay = " +
+                    starting_delay / 1000 + " seg");
+            Thread.sleep(starting_delay);
+            ProcessBuilder pb = new ProcessBuilder(exec_command, file_path);
+            Process start = pb.start();
+            start.waitFor();
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
